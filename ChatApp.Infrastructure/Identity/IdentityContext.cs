11b5b5e0e903ea83941599cb76cd9cab280c79b0
bekirov <vbekirovv@gmail.com>
2022-07-21 12:36:37 +0300

    Added identity entities, identity context.

---
+++ ChatApp.Infrastructure/Identity/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatApp.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Infrastructure.Identity;
+
+public class IdentityContext : IdentityDbContext<UserIdentity, UserRole, int>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    {
+        
+    }
+}
