6c439a5776b63bbe71e2080bd331f1b5bbedc21e
bekirov <vbekirovv@gmail.com>
2022-07-22 20:35:18 +0300

    Added refresh token, token service interface.

---
+++ ChatApp.Application/Common/Interfaces/ITokenService.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+
+namespace ChatApp.Application.Common.Interfaces;
+
+/// <summary>
+/// Represents a token service.
+/// </summary>
+public interface ITokenService
+{
+    /// <summary>
+    /// Generates access token for the specified user.
+    /// </summary>
+    /// <param name="userName">Username.</param>
+    /// <returns>Access token.</returns>
+    string GenerateAccessToken(string userName);
+
+    /// <summary>
+    /// Generates refresh token.
+    /// </summary>
+    /// <returns>Refresh token.</returns>
+    string GenerateRefreshToken();
+
+    /// <summary>
+    /// Generates new access token using refresh token.
+    /// </summary>
+    /// <param name="token">Expired access token.</param>
+    /// <param name="refreshToken">Refresh token.</param>
+    /// <returns>New access token.</returns>
+    string RefreshToken(string token, string refreshToken);
+
+    /// <summary>
+    /// Gets the user principal from the expired access token.
+    /// </summary>
+    /// <param name="token">Access token.</param>
+    /// <returns>User principal.</returns>
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+}
---
+++ ChatApp.Infrastructure/Identity/Entities/RefreshToken.cs
@@ -0,0 +1,37 @@
+﻿namespace ChatApp.Infrastructure.Identity.Entities;
+
+/// <summary>
+/// Represents a refresh token.
+/// </summary>
+public class RefreshToken
+{
+    /// <summary>
+    /// Gets or sets the id for this token.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the token.
+    /// </summary>
+    public string Token { get; set; }
+
+    /// <summary>
+    /// Gets or sets the creation date for this token.
+    /// </summary>
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    /// Gets or sets the expiry date for this token.
+    /// </summary>
+    public DateTime ExpiryDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the flag that indicates whether this token was used.
+    /// </summary>
+    public bool IsUsed { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user id for this token.
+    /// </summary>
+    public int UserId { get; set; }
+}
