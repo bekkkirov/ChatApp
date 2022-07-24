829a1d3b09a888c387d3c694fa3bdd0f8606ff48
bekirov <vbekirovv@gmail.com>
2022-07-24 18:04:53 +0300

    Configured dependency injection for the repositories and unit of work.

--- ChatApp.WebUI/Extensions/ServiceExtensions.cs
+++ ChatApp.WebUI/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
-﻿using ChatApp.Infrastructure.Identity;
+﻿using ChatApp.Application.Common.Persistence;
+using ChatApp.Infrastructure.Identity;
 using ChatApp.Infrastructure.Persistence;
+using ChatApp.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.WebUI.Extensions;
@@ -9,6 +11,7 @@ public static class ServiceExtensions
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     { 
         services.AddDatabaseContexts(configuration);
+        services.AddRepositories();
     }
 
     private static void AddDatabaseContexts(this IServiceCollection services, IConfiguration configuration)
@@ -18,5 +21,16 @@ public static class ServiceExtensions
 
         services.AddDbContext<ChatContext>(opt =>
             opt.UseSqlServer(configuration.GetConnectionString("MainDb")));
+    }
+
+    private static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IImageRepository, ImageRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IChatRepository, ChatRepository>();
+        services.AddScoped<IChannelRepository, ChannelRepository>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 }
