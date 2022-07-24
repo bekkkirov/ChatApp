8f0c9d6665b4265d3fccedd5c4db7dc9e009c596
bekirov <vbekirovv@gmail.com>
2022-07-24 15:59:52 +0300

    Added an interface for the authorization service.

---
+++ ChatApp.Application/Common/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.Application.Common.Models.Authorization;
+
+namespace ChatApp.Application.Common.Interfaces;
+
+public interface IAuthorizationService
+{
+    public Task<TokensModel> SignInAsync(SignInModel signInData);
+
+    public Task<TokensModel> SignUpAsync(SignUpModel signUpData);
+}
