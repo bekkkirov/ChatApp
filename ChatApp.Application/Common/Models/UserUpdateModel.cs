b25892119dfffc4de71f98805c340ef19b7a00b5
bekirov <vbekirovv@gmail.com>
2022-08-02 15:51:03 +0300

    Added model for updating user data.

---
+++ ChatApp.Application/Common/Models/UserUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatApp.Application.Common.Models;
+
+public class UserUpdateModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
