f93003e0ae4a0ae0632163e1bed91f63ff4644fd
bekirov <vbekirovv@gmail.com>
2022-08-03 13:37:13 +0300

    Added validator for the user update model.

---
+++ ChatApp.Application/Common/Validation/User/UserUpdateModelValidator.cs
@@ -0,0 +1,44 @@
+﻿using ChatApp.Application.Common.Models;
+using FluentValidation;
+
+namespace ChatApp.Application.Common.Validation.User;
+
+public class UserUpdateModelValidator : AbstractValidator<UserUpdateModel>
+{
+    public UserUpdateModelValidator()
+    {
+        RuleFor(u => u.FirstName)
+            .NotEmpty()
+            .WithMessage("First name is required.")
+            .MinimumLength(2)
+            .WithMessage("First name must be at least 2 characters.")
+            .MaximumLength(20)
+            .WithMessage("First name can't exceed 20 characters.");
+
+        RuleFor(u => u.LastName)
+            .NotEmpty()
+            .WithMessage("Last name is required.")
+            .MinimumLength(2)
+            .WithMessage("Last name must be at least 2 characters.")
+            .MaximumLength(20)
+            .WithMessage("Last name can't exceed 20 characters.");
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Provided email is not valid.");
+
+        RuleFor(u => u.About)
+            .MaximumLength(100)
+            .WithMessage("About field can't exceed 100 characters");
+
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .WithMessage("Password is required.")
+            .MinimumLength(5)
+            .WithMessage("Password must be at least 5 characters.")
+            .MaximumLength(20)
+            .WithMessage("Password can't exceed 30 characters.");
+    }
+}
