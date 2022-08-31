f6b13b4d38721ccc204fef83f3a4bff0160a0d7f
bekirov <vbekirovv@gmail.com>
2022-08-31 23:20:22 +0300

    Added sign-in component.

---
+++ ChatApp.WebUI/Client/src/app/core/components/sign-in/sign-in.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+    form: FormGroup = new FormGroup({
+        "userName": new FormControl(null, [Validators.required]),
+        "password": new FormControl(null, [Validators.required]),
+    });
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+}
