d8bd2d5a653338900973215f00f62334cebd34c9
bekirov <vbekirovv@gmail.com>
2022-09-03 12:26:21 +0300

    Added authorization logic to the sign in component.

--- ChatApp.WebUI/Client/src/app/core/components/sign-in/sign-in.component.ts
+++ ChatApp.WebUI/Client/src/app/core/components/sign-in/sign-in.component.ts
@@ -1,20 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
     selector: 'app-sign-in',
     templateUrl: './sign-in.component.html',
     styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
     form: FormGroup = new FormGroup({
         "userName": new FormControl(null, [Validators.required]),
         "password": new FormControl(null, [Validators.required]),
     });
 
-    constructor() {
+    constructor(private authService: AuthService,
+                private router: Router,
+                private toastrService: ToastrService) {
     }
 
-    ngOnInit(): void {
+    signIn() {
+        this.authService.signIn(this.form.value).subscribe({
+                complete: () => this.router.navigateByUrl("/"),
+                error: (err) => this.toastrService.error(err.error)
+            }
+        );
     }
 }
