0aaf1dad8413db52459b9496de445d1460a7acd9
bekirov <vbekirovv@gmail.com>
2022-09-03 12:26:41 +0300

    Updated app-component.

--- ChatApp.WebUI/Client/src/app/app.component.ts
+++ ChatApp.WebUI/Client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./core/services/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit{
+    title = 'Client';
+
+    constructor(private authService: AuthService) {
+    }
+
+    ngOnInit(): void {
+        let tokens = localStorage.getItem("tokens");
+
+        if(tokens) {
+            this.authService.setAuthorizationStatus(true);
+        }
+    }
 }
